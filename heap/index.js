a40d692c66924b1d4c2a094bd4b5f3fafc60e828
ianvei <iamianveilleux@gmail.com>
2022-09-04 19:35:36 -0600

    heap remove aka extractMax

--- heap/index.js
+++ heap/index.js
@@ -18,7 +18,32 @@ class MaxBinaryHeap{
             index = parent;
             parent = Math.floor(((parent-1) / 2));
         }
-        console.log(this.values);
+        return this.values;
+    }
+
+    extractMax(){
+        let temp = this.values[0];
+        this.values[0] = this.values[this.values.length-1];
+        this.values[this.values.length-1] = temp;
+        this.values.pop();
+
+        let index = 0;
+        while(true){
+            let firstChild = this.values[2 * index + 1]
+            let secondChild = this.values[2 * index + 2]
+            let swapIndex = firstChild > secondChild? 2 * index + 1 : 2 * index + 2;
+
+            if (this.values[index] < this.values[swapIndex]){
+                console.log(this.values[index], this.values[swapIndex])
+                let temp = this.values[index];
+                this.values[index] = this.values[swapIndex];
+                this.values[swapIndex] = temp;
+                index = swapIndex;
+            } else {
+                console.log(this.values)
+                return false;
+            }
+        }
     }
 }
 
@@ -26,7 +51,10 @@ class MaxBinaryHeap{
 
 const heap = new MaxBinaryHeap();
 heap.values = [41,39,33,18,27,12];
-heap.insert(55);
+// heap.insert(55);
 // heap.insert(35);
-heap.insert(1);
-heap.insert(45);
+// heap.insert(1);
+// heap.insert(45);
+// heap.insert(199);
+console.log(heap.values)
+heap.extractMax();
