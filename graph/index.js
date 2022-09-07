61d87d8d18c834f7e23ac1e3e4d5f9223ca6e400
ianvei <iamianveilleux@gmail.com>
2022-09-06 18:00:21 -0600

    graph traversal

--- graph/index.js
+++ graph/index.js
@@ -1,6 +1,7 @@
 class Graph{
     constructor(){
         this.adjacencyList = {};
+        this.visited = {}
     }
 
     addVertex(key){
@@ -8,6 +9,13 @@ class Graph{
             this.adjacencyList[key] = [];
         }
         console.log(this.adjacencyList);
+    }
+
+    generateVisited(){
+        for(let vert in this.adjacencyList){
+            this.visited[vert] = false;
+        }
+        console.log(this.visited)
     }
 
     addEdge(vertex1, vertex2){
@@ -31,19 +39,87 @@ class Graph{
         delete this.adjacencyList[vertex];
         console.log(this.adjacencyList)
     }
+
+    depthFirstRecursive(vertex){
+        this.generateVisited();
+        let adjacentNodes = [];
+
+        const depthFirstHelper = (vertex) => {
+            if(!vertex){
+                return;
+            }
+            adjacentNodes.push(vertex)
+            this.visited[vertex] = true;
+            for(let vert of this.adjacencyList[vertex]){
+                    if(!this.visited[vert]){
+                        depthFirstHelper(vert);
+                    }
+            }   
+        }
+        depthFirstHelper(vertex);
+        console.log(adjacentNodes)
+        return adjacentNodes
+    }
+
+    depthFirstIterative(start){
+        let stack = []
+        let result = []
+        stack.push(start);
+        
+        while(stack.length > 0){
+            let currentVert = stack.pop();
+            if(!this.visited[currentVert]){
+                this.visited[currentVert] = true;
+                result.push(currentVert)
+                for(let vert of this.adjacencyList[currentVert]){
+                    stack.push(vert)
+                }
+            }
+
+        }
+        console.log(result)
+    }
+
+    breadthFirst(start){
+        let stack = []
+        let result = []
+        stack.push(start);
+        let currentVert = start;
+        while(stack.length > 0){
+            currentVert = stack.shift()
+            if(!this.visited[currentVert]){
+                this.visited[currentVert] = true;
+                result.push(currentVert)
+                for(let vert of this.adjacencyList[currentVert]){
+                    stack.push(vert)
+                }
+            }
+            
+            
+        }
+        console.log(result);
+    }
 }
 
 let graph = new Graph();
 
-graph.addVertex('Tokyo');
-graph.addVertex('Dallas');
-graph.addVertex('Aspen');
-graph.addVertex('Toronto');
-graph.addEdge('Tokyo', 'Aspen')
-graph.addEdge('Tokyo', 'Dallas')
-graph.addEdge('Dallas', "Aspen")
-graph.addEdge('Toronto', "Aspen")
-graph.addEdge('Toronto', "Tokyo")
-graph.removeVertex("Tokyo")
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+graph.addEdge('A', 'B')
+graph.addEdge('A', 'C')
+graph.addEdge('B', "D")
+graph.addEdge('C', "E")
+graph.addEdge('D', "E")
+graph.addEdge('D', "F")
+graph.addEdge('E', "F")
+// graph.generateVisited();
+// graph.depthFirst('A');
+graph.breadthFirst("A");
+
+// graph.removeVertex("Tokyo")
 // graph.removeEdge('Tokyo', 'Aspen')
 // graph.removeEdge('Tokyo', 'Dallas')
